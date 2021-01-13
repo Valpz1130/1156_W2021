7dcf210d009c48cd6c9999e5d13c30c0480ad374
Michael Feeney <michael.feeney@badmonster.com>
2021-01-12 19:51:06 -0500

    Code as it was at the end of the lecture.

---
+++ CPPIsTheBest/CPPIsTheBest/theMain.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  // C++ IO
+
+// Standard Template Library STL
+// Everything starts with std:: ("scope" to a "namespace")
+// 
+// You are welcome to do this, if you'd like.
+// But I will not do this.
+//using namespace std;
+
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+
+	return 0;
+}
+
+
